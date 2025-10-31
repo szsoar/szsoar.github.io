978c5e5c8a246fd4893f2d17b9421bf242db9916
szsoar <hlm_0904@163.com>
2025-10-31 15:34:02 +0800

    add software list

---
+++ content/software/js/category-data.js
@@ -0,0 +1,43 @@
+// AE表达式分类文章数据
+const softwareArticles = [
+  {
+    id: 1,
+    title: "AE脚本UI设计器安装激活向导",
+    category: "软件指南",
+    description: "",
+    image: "../../assets/images/software-articles/20251030-170001.jpg",
+    url: "scriptui-setup.html",
+    date: "2025-09-29"
+  },
+
+  {
+    id: 2,
+    title: "AE脚本UI设计器安装激活向导",
+    category: "UXP开发",
+    description: "",
+    image: "../../assets/images/software-articles/20251030-170002.jpg",
+    url: "plugin-lead.html",
+    date: "2025-09-29"
+  },
+
+  {
+    id: 3,
+    title: "AE脚本UI设计器安装激活向导",
+    category: "UXP开发",
+    description: "",
+    image: "../../assets/images/software-articles/20251101-190001.jpg",
+    url: "plugin-lead.html",
+    date: "2025-09-29"
+  },
+
+  {
+    id: 4,
+    title: "AE脚本UI设计器安装激活向导",
+    category: "UXP开发",
+    description: "",
+    image: "../../assets/images/software-articles/20251101-190002.jpg",
+    url: "plugin-lead.html",
+    date: "2025-09-29"
+  },
+
+];
