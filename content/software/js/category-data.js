ca93366b664a7e54a6519e51d7d12bcd0c18bda8
szsoar <hlm_0904@163.com>
2025-11-02 10:07:15 +0800

    edit scriptui designer 01

--- content/software/js/category-data.js
+++ content/software/js/category-data.js
@@ -7,23 +7,23 @@ const softwareArticles = [
     description: "",
     image: "../../assets/images/software-articles/20251030-170001.jpg",
     url: "scriptui-setup.html",
-    date: "2025-09-29"
+    date: "2025-10-29"
   },
 
   {
     id: 2,
-    title: "AE脚本UI设计器安装激活向导",
-    category: "UXP开发",
+    title: "AE脚本UI设计器使用指南",
+    category: "软件指南",
     description: "",
     image: "../../assets/images/software-articles/20251030-170002.jpg",
     url: "plugin-lead.html",
-    date: "2025-09-29"
+    date: "2025-10-29"
   },
 
   {
     id: 3,
     title: "AE脚本UI设计器安装激活向导",
-    category: "UXP开发",
+    category: "软件指南",
     description: "",
     image: "../../assets/images/software-articles/20251101-190001.jpg",
     url: "plugin-lead.html",
@@ -33,7 +33,7 @@ const softwareArticles = [
   {
     id: 4,
     title: "AE脚本UI设计器安装激活向导",
-    category: "UXP开发",
+    category: "软件指南",
     description: "",
     image: "../../assets/images/software-articles/20251101-190002.jpg",
     url: "plugin-lead.html",
